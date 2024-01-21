00d9e1eafa8f7ee15ec588d57b0754d975dd0f9d
siyarp97 <siyarp97@gmail.com>
2024-01-21 22:13:44 +0300

    Returning true result issue fixed but try to add some other properties kind of keep doing calculate process over and over again

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const screen = document.querySelector('.screen')
 const numberKeys = [...document.querySelectorAll('.numberKeys')]
 const operateKeys = [...document.querySelectorAll('.key-event')]
+const enter = document.querySelector('.enter')
+
 
 
 const calc = {
@@ -8,14 +10,14 @@ const calc = {
     firstNumber : '',
     secondNumber : '',
     operation : '',
-    result: 0
+    result: 0,
 }
 
 
 
 keyListener()
 definer()
-
+equals()
 
 
 
@@ -24,60 +26,99 @@ function keyListener(){
     numberKeys.forEach((key) => {
         key.addEventListener('click', () => {
             calc.keyListener += (key.innerText)
-            console.log(calc)
+
             if(calc.firstNumber){
                 calc.secondNumber = calc.keyListener
-                return screen.innerHTML = 
-                `
-                    <span class="first-number">${calc.firstNumber}
-                    <span class="operator">${calc.operation}</span>
-                    </span>
-                    <span class="second-number">${calc.keyListener}</span>
-                `
+                console.log(calc)
             }
-            else{
-            return  screen.innerText = calc.keyListener
-            }
+
+
+            display()
         })
     })
 }
 
-function firstNumber(){
-    calc.firstNumber = calc.keyListener
-    calc.keyListener = ''
-    calc.secondNumber = calc.keyListener
-}
+// function firstNumber(){
+//     calc.firstNumber = calc.keyListener
+//     calc.keyListener = ''
+// }
 
 
 function definer(){
     operateKeys.forEach((opKey) =>{
+
         opKey.addEventListener('click', () => {
-                firstNumber()
-                calc.operation = opKey.innerText
                 screen.classList.add('screen-display')
+                    calc.firstNumber = calc.keyListener
+                    calc.keyListener = ''
 
-                if(opKey.innerText == '='){ 
-                    return screen.innerHTML = 
-                    `
-                    <span class="operator">${calc.operation}</span>
-                    </span>
-                    <span class="second-number">${calc.result}</span>
-                    `
-                }
-
-                else{
+                    calc.operation = opKey.innerText
+                    console.log(calc)
+                    return screen.innerHTML = display()
                 
-                    return screen.innerHTML = 
-                    `
-                    <span class="first-number">${calc.firstNumber}
-                    <span class="operator">${calc.operation}</span>
-                    </span>
-                    <span class="second-number">${calc.keyListener}</span>
-                    `
-                }
-        })
+            
+            }
+        )
     })
 }
+
+
+
+function operate(operator, firstNumber, secondNumber){
+    operator = calc.operation
+    firstNumber = calc.firstNumber
+    secondNumber = calc.secondNumber
+    switch(operator){
+        case '+':
+            calc.result = add(calc.firstNumber, calc.secondNumber)
+            return calc.result;
+        
+        case '-':
+            calc.result = subtract(calc.firstNumber, calc.secondNumber)
+            return calc.result;
+        
+        case 'x':
+            calc.result = multiply(calc.firstNumber, calc.secondNumber)
+            return calc.result;
+        
+        case '/':
+            calc.result = divide(calc.firstNumber, calc.secondNumber)
+            return calc.result;
+        
+    }
+}
+
+
+function display(){
+
+    if(calc.firstNumber){
+        return screen.innerHTML = 
+        `
+            <span class="first-number">${calc.firstNumber}
+            <span class="operator">${calc.operation}</span>
+            </span>
+            <span class="second-number">${calc.secondNumber}</span>
+        `
+    }
+
+    else{
+        return  screen.innerText = calc.keyListener
+    }
+}
+
+function equals(){
+    enter.addEventListener('click', () => {
+        operate()
+        console.log(calc)
+        return screen.innerHTML = 
+        `
+            <span class="operator">=</span>
+            </span>
+            <span class="second-number">${calc.result}</span>
+        `
+    })
+}
+
 
 
 
@@ -97,3 +138,24 @@ function divide (x,y){
     return (x / y).toFixed(2)
 }
 
+
+
+
+
+
+//function operate(operator){
+    //     operator = calc.operation
+    //     switch(operator){
+    //         case '+':
+    //             return calc.result = add(calc.firstNumber, calc.secondNumber);
+    //         case '-':
+    //             return subtract(calc.firstNumber, calc.secondNumber);
+    //         case 'x':
+    //                 return subtract(calc.firstNumber, calc.secondNumber);
+    //         case '/':
+    //             return subtract(calc.firstNumber, calc.secondNumber);
+    //         case '=':
+    //             return console.log('D')
+    //     }
+    // console.log(calc.result)
+    // }
